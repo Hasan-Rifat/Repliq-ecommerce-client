8542b451f9f83601a789f5c39f6e9e5f89c908f2
Hasan-Rifat <developerhr000@gmail.com>
2023-08-14 00:14:51 +0600

    [loading update]

--- src/shared/Loading.tsx
+++ src/shared/Loading.tsx
@@ -4,8 +4,10 @@ type LoadingProps = {};
 
 const Loading: React.FC<LoadingProps> = () => {
   return (
-    <div className="mx-auto w-[80vw] h-[20vh] flex items-center justify-center">
-      <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-violet-400 mx-auto"></div>
+    <div className="max-w-[1200px] mx-auto flex px-5 py-24">
+      <div className="mx-auto w-[80vw] h-[20vh] flex items-center justify-center">
+        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-violet-400 mx-auto"></div>
+      </div>
     </div>
   );
 };
